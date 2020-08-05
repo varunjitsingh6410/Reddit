84cee9fdb238bdb6f2fef4cf812b5de43e85f5d3
Varun Jit Singh <varunjitsingh6410@gmail.com>
2020-08-05 15:13:29 -0700

    Implement navigation login to main

--- app/src/main/java/com/example/reddit/LoginActivity.java
+++ app/src/main/java/com/example/reddit/LoginActivity.java
@@ -1,14 +1,39 @@
 package com.example.reddit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnLogin;
+    EditText etUsername;
+    EditText etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
