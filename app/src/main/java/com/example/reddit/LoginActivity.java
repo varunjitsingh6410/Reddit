61a4426b19fed51c61026ed4ad70d06db9d21251
Varun Jit Singh <varunjitsingh6410@gmail.com>
2020-08-06 13:31:40 -0700

    Remove unneeded ui elements on login

--- app/src/main/java/com/example/reddit/LoginActivity.java
+++ app/src/main/java/com/example/reddit/LoginActivity.java
@@ -11,8 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin;
-    EditText etUsername;
-    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +18,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.btnLogin);
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goMainActivity();
+                //goMainActivity();
             }
         });
     }
