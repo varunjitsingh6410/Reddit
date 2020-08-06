0c5cdd7ccb975cdc684f0e76ef0ce0eae6d43949
Varun Jit Singh <varunjitsingh6410@gmail.com>
2020-08-06 16:26:53 -0700

    Implement Reddit client login

--- app/src/main/java/com/example/reddit/LoginActivity.java
+++ app/src/main/java/com/example/reddit/LoginActivity.java
@@ -1,35 +1,125 @@
 package com.example.reddit;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    Button btnLogin;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class LoginActivity extends AppCompatActivity {
+    
+    public static final String TAG = "LoginActivity";
+
+    private static final String AUTH_URL =
+            "https://www.reddit.com/api/v1/authorize.compact?client_id=%s" +
+                    "&response_type=code&state=%s&redirect_uri=%s&" +
+                    "duration=permanent&scope=identity";
+
+    private static final String CLIENT_ID = "t8TFskzpVhTGCQ";
+
+    private static final String REDIRECT_URI =
+            "https://www.reddit.com/";
+
+    private static final String STATE = "MY_RANDOM_STRING_1";
+
+    private static final String ACCESS_TOKEN_URL =
+            "https://www.reddit.com/api/v1/access_token";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        btnLogin = findViewById(R.id.btnLogin);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //goMainActivity();
-            }
-        });
     }
 
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    public void startSignIn(View view) {
+        String url = String.format(AUTH_URL, CLIENT_ID, STATE, REDIRECT_URI);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(getIntent()!=null && getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            Uri uri = getIntent().getData();
+            if(uri.getQueryParameter("error") != null) {
+                String error = uri.getQueryParameter("error");
+                Log.e(TAG, "An error has occurred : " + error);
+            } else {
+                String state = uri.getQueryParameter("state");
+                if(state.equals(STATE)) {
+                    String code = uri.getQueryParameter("code");
+                    getAccessToken(code);
+                    goMainActivity();
+                }
+            }
+        }
+    }
+
+    private void getAccessToken(String code) {
+        OkHttpClient client = new OkHttpClient();
+
+        String authString = CLIENT_ID + ":";
+        String encodedAuthString = Base64.encodeToString(authString.getBytes(),
+                Base64.NO_WRAP);
+
+        Request request = new Request.Builder()
+                .addHeader("User-Agent", "Sample App")
+                .addHeader("Authorization", "Basic " + encodedAuthString)
+                .url(ACCESS_TOKEN_URL)
+                .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"),
+                        "grant_type=authorization_code&code=" + code +
+                                "&redirect_uri=" + REDIRECT_URI))
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "ERROR: " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+
+                JSONObject data = null;
+                try {
+                    data = new JSONObject(json);
+                    String accessToken = data.optString("access_token");
+                    String refreshToken = data.optString("refresh_token");
+
+                    Log.d(TAG, "Access Token = " + accessToken);
+                    Log.d(TAG, "Refresh Token = " + refreshToken);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
